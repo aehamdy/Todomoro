dd6b21d728af86e90b5d4eeeca55ce813a693540
aehamdy <ae.hamdy91@gmail.com>
2024-08-08 17:05:20 +0300

    feat: adding button

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 
 function InputForm() {
   const [userInput, setUserInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
+  const handleClick = () => {
+    setTasks([userInput, ...tasks]);
+  };
+
   return (
     <section>
       <input type="text" value={userInput} onChange={handleInputChange} />
+      <button type="button" onClick={handleClick}>
+        Add Task
+      </button>
     </section>
   );
 }
