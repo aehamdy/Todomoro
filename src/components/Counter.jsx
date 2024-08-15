21f0d319ad94f28dde1e220e631b111bc0386448
aehamdy <ae.hamdy91@gmail.com>
2024-08-15 22:25:57 +0300

    feat: start run timer upon clicking on 'Start' button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,9 +2,26 @@ import { useState } from "react";
 
 function Counter() {
   const [value, setValue] = useState(0);
+  const [timer, setTimer] = useState({ minutes: 0, seconds: 0 });
 
   const onInputChange = (e) => {
     setValue(e.target.value);
+  };
+
+  const onStartTimer = () => {
+    setTimer({ minutes: value - 1, seconds: 59 });
+
+    setInterval(() => {
+      setTimer((prevValue) => {
+        const { minutes, seconds } = prevValue;
+
+        if (seconds === 0) {
+          return { minutes: timer - 1, seconds: 59 };
+        } else {
+          return { minutes: minutes, seconds: seconds - 1 };
+        }
+      });
+    }, 1000);
   };
 
   return (
@@ -15,9 +32,14 @@ function Counter() {
         onChange={onInputChange}
         className="bg-slate-300"
       />
-      <button type="button" className="text-black">
+      <button type="button" onClick={onStartTimer} className="text-black">
         Start
       </button>
+      <div className="text-black">
+        <div>00</div>
+        <div>{timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes}</div>
+        <div>{timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds}</div>
+      </div>
     </section>
   );
 }
