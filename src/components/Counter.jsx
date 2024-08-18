10b6c4ab815335eafe5f287f2dfa1b914dca400d
aehamdy <ae.hamdy91@gmail.com>
2024-08-18 21:25:00 +0300

    feat: Added 'tick' sound to the timer with mute/unmute button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,40 @@
 /* eslint-disable react/no-unescaped-entities */
-import tickSound from "../sounds/1.wav";
+import tickSound from "../sounds/tick-sound.wav";
 import { useEffect, useRef, useState } from "react";
+
+const iconSize = 20;
+
+const speakerOn = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={iconSize}
+    fill="none"
+    viewBox="0 0 28 28"
+  >
+    <path
+      fill="#212121"
+      d="M16.5 4.814c0-1.094-1.307-1.66-2.105-.912l-4.937 4.63a1.75 1.75 0 0 1-1.197.473H5.25A3.25 3.25 0 0 0 2 12.255v3.492a3.25 3.25 0 0 0 3.25 3.25h3.012c.444 0 .872.17 1.196.473l4.937 4.626c.799.749 2.105.183 2.105-.912V4.814Zm-6.016 4.812L15 5.391v17.216l-4.516-4.231a3.25 3.25 0 0 0-2.222-.879H5.25a1.75 1.75 0 0 1-1.75-1.75v-3.492c0-.966.784-1.75 1.75-1.75h3.011a3.25 3.25 0 0 0 2.223-.879ZM22.702 5.252a.75.75 0 0 0-1.126.99A11.702 11.702 0 0 1 24.5 14c0 2.973-1.103 5.687-2.924 7.757a.75.75 0 1 0 1.126.99A13.202 13.202 0 0 0 26 14c0-3.352-1.245-6.415-3.298-8.748Z"
+    />
+    <path
+      fill="#212121"
+      d="M20.353 8.303a.75.75 0 1 0-1.2.9A7.961 7.961 0 0 1 20.75 14c0 1.8-.594 3.46-1.597 4.797a.75.75 0 1 0 1.2.9A9.461 9.461 0 0 0 22.25 14c0-2.136-.706-4.11-1.897-5.697Z"
+    />
+  </svg>
+);
+
+const speakerOff = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={iconSize}
+    fill="none"
+    viewBox="0 0 28 28"
+  >
+    <path
+      fill="#212121"
+      d="M14.395 3.902c.798-.748 2.105-.182 2.105.912v18.37c0 1.095-1.306 1.66-2.105.912L9.458 19.47a1.75 1.75 0 0 0-1.196-.473H5.25A3.25 3.25 0 0 1 2 15.747v-3.492a3.25 3.25 0 0 1 3.25-3.25h3.011c.445 0 .873-.169 1.197-.473l4.937-4.63ZM15 5.392l-4.516 4.234a3.25 3.25 0 0 1-2.223.88H5.25a1.75 1.75 0 0 0-1.75 1.75v3.491c0 .967.784 1.75 1.75 1.75h3.012c.825 0 1.62.314 2.222.879L15 22.607V5.391ZM19.782 10.722a.75.75 0 0 0-1.064 1.056l2.218 2.235-2.215 2.206a.75.75 0 1 0 1.058 1.062l2.218-2.207 2.225 2.208a.75.75 0 0 0 1.056-1.064l-2.22-2.205 2.224-2.234a.75.75 0 1 0-1.063-1.058l-2.223 2.231-2.214-2.23Z"
+    />
+  </svg>
+);
 
 function Counter() {
   const [value, setValue] = useState(0);
@@ -8,11 +42,16 @@ function Counter() {
   const [isTimerFinished, setIsTimerFinished] = useState(false);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
-  const timerInterval = useRef(null);
+  const [isSpeakerOn, setIsSpeakerOn] = useState(true);
+  const timerRef = useRef(null);
+  const audioRef = useRef(new Audio(tickSound));
+
+  const speaker = isSpeakerOn ? speakerOn : speakerOff;
 
   function playSound() {
-    let beat = new Audio(tickSound);
-    beat.play();
+    if (isSpeakerOn) {
+      audioRef.current.play();
+    }
   }
 
   const onInputChange = (e) => {
@@ -24,12 +63,12 @@ function Counter() {
     setIsTimerFinished(false);
     setIsTimerRunning(true);
 
-    timerInterval.current = setInterval(() => {
+    timerRef.current = setInterval(() => {
       setTimer((prevValue) => {
         const { minutes, seconds } = prevValue;
 
         if (minutes === 0 && seconds === 0) {
-          clearInterval(timerInterval.current);
+          clearInterval(timerRef.current);
           setIsTimerFinished(true);
           setIsTimerRunning(false);
           return { minutes: 0, seconds: 0 };
@@ -45,19 +84,20 @@ function Counter() {
   };
 
   const onPauseClick = () => {
-    clearInterval(timerInterval.current);
+    clearInterval(timerRef.current);
     setIsPaused(!isPaused);
+    setIsSpeakerOn((prevValue) => prevValue);
   };
 
   const onPlayClick = () => {
     setTimer({ minutes: timer.minutes, seconds: timer.seconds });
 
-    timerInterval.current = setInterval(() => {
+    timerRef.current = setInterval(() => {
       setTimer((prevValue) => {
         const { minutes, seconds } = prevValue;
 
         if (minutes === 0 && seconds === 0) {
-          clearInterval(timerInterval.current);
+          clearInterval(timerRef.current);
           setIsTimerFinished(true);
           setIsTimerRunning(false);
           return { minutes: 0, seconds: 0 };
@@ -69,13 +109,14 @@ function Counter() {
       });
 
       playSound();
+      setIsSpeakerOn((prevValue) => prevValue);
     }, 1000);
 
     setIsPaused(!isPaused);
   };
 
   const onStopClick = () => {
-    clearInterval(timerInterval.current);
+    clearInterval(timerRef.current);
     setTimer({ minutes: 0, seconds: 0 });
 
     setTimeout(() => {
@@ -83,8 +124,17 @@ function Counter() {
     }, 200);
   };
 
+  const onSpeakerClick = () => {
+    setIsSpeakerOn((prev) => !prev);
+    if (isSpeakerOn) {
+      audioRef.current.muted = true;
+    } else {
+      audioRef.current.muted = false;
+    }
+  };
+
   useEffect(() => {
-    return () => clearInterval(timerInterval.current);
+    return () => clearInterval(timerRef.current);
   }, []);
 
   return (
@@ -138,10 +188,20 @@ function Counter() {
       {isTimerFinished ? (
         <div className="p-1 text-red-500">TIME'S UP!</div>
       ) : (
-        <div className="flex justify-center text-2xl text-black">
-          <div>{timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes} </div>
-          <div> {" : "} </div>
-          <div> {timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds}</div>
+        <div className="flex justify-evenly items-center">
+          <div className="flex justify-center text-2xl text-black">
+            <div>
+              {timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes}{" "}
+            </div>
+            <div> {" : "} </div>
+            <div>
+              {" "}
+              {timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds}
+            </div>
+          </div>
+          <div onClick={onSpeakerClick} className="speaker-icon cursor-pointer">
+            {speaker}
+          </div>
         </div>
       )}
     </section>
@@ -153,7 +213,8 @@ export default Counter;
 /*
  # [x] show a 'pause' and 'stop" buttons when the timer starts
  # [x] hide the input and the 'start' button when timer starts
- # [ ] add a clock sound while timer is counting
+ # [x] add a clock sound while timer is counting
  # [ ] add mute button to mute that sound
+ # [ ] handle input validation when insert 0
  # [ ] enlarge the minutes and seconds and add proper styling
  */
