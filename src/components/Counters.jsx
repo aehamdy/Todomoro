aed1fdbd2ce27b0229203003a57eadb2e61464c1
aehamdy <ae.hamdy91@gmail.com>
2024-08-28 15:17:58 +0300

    feat: Add rendering selected timer upon input check

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import PomodoroTimer from "./PomodoroTimer";
 import Timer from "./Timer";
 
 function Counters() {
+  const [selectedTimer, setSelectedTimer] = useState();
+
+  const handleTimerChange = (e) => {
+    setSelectedTimer(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <section>
       <div>
@@ -11,17 +19,24 @@ function Counters() {
             id="pomodoro"
             name="selected_timer"
             value="pomodoro"
+            checked={selectedTimer === "pomodoro"}
+            onChange={handleTimerChange}
           />
           Pomodoro
         </label>
         <label htmlFor="timer">
-          <input type="radio" id="timer" name="selected_timer" value="timer" />
+          <input
+            type="radio"
+            id="timer"
+            name="selected_timer"
+            value="timer"
+            checked={selectedTimer === "timer"}
+            onChange={handleTimerChange}
+          />
           Timer
         </label>
       </div>
-      <div>
-        <PomodoroTimer /> <Timer />
-      </div>
+      <div>{selectedTimer === "pomodoro" ? <PomodoroTimer /> : <Timer />}</div>
     </section>
   );
 }
