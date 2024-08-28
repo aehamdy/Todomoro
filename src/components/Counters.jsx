ef3334dc79bbcc2ccff46c629cbf94b82fbdf575
aehamdy <ae.hamdy91@gmail.com>
2024-08-28 16:01:12 +0300

    style: styled Counters component

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -7,16 +7,19 @@ function Counters() {
 
   const handleTimerChange = (e) => {
     setSelectedTimer(e.target.value);
-    console.log(e.target.value);
   };
+
+  // Colors: #91915B #FFCB94 #FFEBB5 #F87C4D #A4404C
+  // Colors: #FCC201 #DA9101 #940D21 #CF2121 #ED5742
+  // Colors: #C4F0C5 #F1FFF1 #9EBFCB
 
   return (
     <section>
-      <div className="w-fit flex items-center gap-1 py-0 px-0 mx-auto text-black bg-blue-300 rounded-lg">
+      <div className="w-fit flex items-center gap-1 py-0 px-0 mx-auto text-black bg-[#fffafa] rounded-lg">
         <label
           htmlFor="pomodoro"
           className={`rounded-s-lg py-1 px-2 ${
-            selectedTimer === "pomodoro" && "bg-red-500 text-white"
+            selectedTimer === "pomodoro" && "bg-[#F87C4D] text-white"
           } cursor-pointer`}
         >
           <input
@@ -26,16 +29,14 @@ function Counters() {
             value="pomodoro"
             checked={selectedTimer === "pomodoro"}
             onChange={handleTimerChange}
-            className={`appearance-none ${
-              selectedTimer === "pomodoro" && "bg-red-400 text-white"
-            }`}
+            className="appearance-none"
           />
           Pomodoro
         </label>
         <label
           htmlFor="timer"
           className={`rounded-e-lg py-1 px-2 ${
-            selectedTimer === "timer" && "bg-red-500 text-white"
+            selectedTimer === "timer" && "bg-[#ff5945] text-white"
           } cursor-pointer`}
         >
           <input
@@ -50,7 +51,7 @@ function Counters() {
           Timer
         </label>
       </div>
-      <div>
+      <div className="my-3">
         {selectedTimer === "pomodoro" ? (
           <PomodoroTimer />
         ) : selectedTimer === "timer" ? (
