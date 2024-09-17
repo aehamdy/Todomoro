b2e49161274ed83708b5d7574117a53c04bc5ea2
aehamdy <ae.hamdy91@gmail.com>
2024-09-17 19:50:16 +0300

    chore: Changed todo ticked icon to gray color

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -187,14 +187,14 @@ const tickedCircleIcon = (
     viewBox="0 0 24 24"
   >
     <path
-      stroke="#292D32"
+      stroke="#928e85"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth="1.5"
       d="M12 22c5.5 0 10-4.5 10-10S17.5 2 12 2 2 6.5 2 12s4.5 10 10 10Z"
     />
     <path
-      stroke="#292D32"
+      stroke="#928e85"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth="1.5"
