fcbb27dc1be9461400abfa8f5bf7016192e3e67b
aehamdy <ae.hamdy91@gmail.com>
2024-09-24 23:35:29 +0300

    style: Changed the style of checked category option and style upon hover

--- src/components/TodoCategoryOptions.jsx
+++ src/components/TodoCategoryOptions.jsx
@@ -15,7 +15,7 @@ function TodoCategoryOptions(props) {
   };
 
   return (
-    <ul className="flex items-center gap-3">
+    <ul className="flex items-center gap-2">
       {categoryOptions.map((option, i) => (
         <li key={i}>
           <input
@@ -28,7 +28,7 @@ function TodoCategoryOptions(props) {
           />
           <label
             htmlFor={option.id}
-            className="inline-flex items-center justify-between w-full p-2 text-gray-500 hover:text-black bg-white hover:bg-gray-100 border border-gray-200 hover:border-blue-300 rounded-lg peer-checked:border-gray-400 peer-checked:text-blue-600 cursor-pointer"
+            className="inline-flex items-center justify-between w-full p-2 text-gray-500 hover:text-black peer-checked:text-blue-500 peer-checked:font-bold bg-white hover:bg-gray-100 border peer-checked:bg-gray-100 border-gray-200 hover:border-gray-300 peer-checked:border-blue-300 rounded-lg cursor-pointer"
           >
             {option.value.charAt(0).toUpperCase() + option.value.slice(1)}
           </label>
