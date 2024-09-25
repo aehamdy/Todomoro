37bcdae2e06382c2d671a340707d13568ce2599c
aehamdy <ae.hamdy91@gmail.com>
2024-09-25 22:32:45 +0300

    chore: Displayed icons for TodoCategoryOptions and formatted the value of TodoCategoryButtons

--- src/components/TodoCategoryOptions.jsx
+++ src/components/TodoCategoryOptions.jsx
@@ -32,6 +32,7 @@ function TodoCategoryOptions(props) {
             htmlFor={option.id}
             className="inline-flex items-center justify-between gap-2 w-full p-2 text-gray-500 hover:text-black peer-checked:text-blue-500 peer-checked:font-bold bg-white hover:bg-gray-100 border peer-checked:bg-gray-100 border-gray-200 hover:border-gray-300 peer-checked:border-blue-300 rounded-lg cursor-pointer"
           >
+            <span>{option.icon}</span>
             {option.value.charAt(0).toUpperCase() + option.value.slice(1)}
           </label>
         </li>
