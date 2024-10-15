82982128c455499ba363f80b4cda19dc3a239ec7
aehamdy <ae.hamdy91@gmail.com>
2024-10-15 22:35:56 +0300

    style: Add increment and decrement icons

--- src/components/TimerInput.jsx
+++ src/components/TimerInput.jsx
@@ -1,6 +1,42 @@
 /* eslint-disable react/prop-types */
-
 import { useState } from "react";
+
+const iconSize = "30";
+const strokeWidth = "1.6";
+
+const plusIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={iconSize}
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={strokeWidth}
+      d="M12 16v-3m0 0v-3m0 3H9m3 0h3m6-7-2-2m-9-2h4m-2 19a8 8 0 1 1 0-16 8 8 0 0 1 0 16Z"
+    />
+  </svg>
+);
+
+const minusIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={iconSize}
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={strokeWidth}
+      d="M9 13h6m6-7-2-2m-9-2h4m-2 19a8 8 0 1 1 0-16 8 8 0 0 1 0 16Z"
+    />
+  </svg>
+);
 
 function TimerInput(props) {
   const { setError, onValueChange } = props;
@@ -21,12 +57,15 @@ function TimerInput(props) {
   };
 
   return (
-    <input
-      type="number"
-      value={value}
-      onChange={onInputChange}
-      className="w-16 py-1 p-2 text-[#222] bg-white rounded-md hover:shadow-lg focus:outline-none"
-    />
+    <div className="flex justify-between items-center gap-4 divide-x-2 text-black rounded-md">
+      <input
+        type="text"
+        value={value}
+        onChange={onInputChange}
+        className="w-12 py-1 px-2 text-[#222] bg-white rounded-md focus:outline-none"
+        required
+      />
+    </div>
   );
 }
 
