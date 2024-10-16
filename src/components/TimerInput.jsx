80e651e15f0b9863295733cc2b85bc82f501cb60
aehamdy <ae.hamdy91@gmail.com>
2024-10-16 15:11:30 +0300

    style: Adjust styling of increment and decrement buttons

--- src/components/TimerInput.jsx
+++ src/components/TimerInput.jsx
@@ -1,43 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-const iconSize = "30";
-const strokeWidth = "1.6";
-
-const plusIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={iconSize}
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <path
-      stroke="#000"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={strokeWidth}
-      d="M12 16v-3m0 0v-3m0 3H9m3 0h3m6-7-2-2m-9-2h4m-2 19a8 8 0 1 1 0-16 8 8 0 0 1 0 16Z"
-    />
-  </svg>
-);
-
-const minusIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={iconSize}
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <path
-      stroke="#000"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={strokeWidth}
-      d="M9 13h6m6-7-2-2m-9-2h4m-2 19a8 8 0 1 1 0-16 8 8 0 0 1 0 16Z"
-    />
-  </svg>
-);
-
 function TimerInput(props) {
   const { setError, onValueChange } = props;
   const [value, setValue] = useState(0);
@@ -76,7 +39,7 @@ function TimerInput(props) {
     <div className="flex justify-between items-center gap-4 divide-x-2 text-black rounded-md">
       <button
         onClick={decreaseTimer}
-        className="py-1 px-2 text-white bg-gray-700 hover:bg-gray-600 rounded-md duration-short"
+        className="py-1 px-3 text-lg text-white bg-gray-700 hover:bg-gray-600 active:bg-gray-500 rounded-md duration-short"
       >
         -
       </button>
@@ -89,7 +52,7 @@ function TimerInput(props) {
       />
       <button
         onClick={increaseTimer}
-        className="py-1 px-2 text-white bg-gray-700 hover:bg-gray-600 rounded-md duration-short"
+        className="py-1 px-3 text-lg text-white bg-gray-700 hover:bg-gray-600 active:bg-gray-500 rounded-md duration-short"
       >
         +
       </button>
