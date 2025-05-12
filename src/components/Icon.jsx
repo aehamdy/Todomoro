8ecca1d1b5ef494e759427ab2296a76f2557301c
AhmedEssam <ae.devverse@gmail.com>
2025-05-12 23:14:32 +0300

    Feat: Create Icon component

---
+++ src/components/Icon.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+const iconMap = {};
+
+function Icon({ name, size, className = "" }) {
+  const IconComponent = iconMap[name];
+
+  if (!IconComponent) {
+    console.warn(
+      `Icon ${name} is not defined in iconMap. Check you name prop or iconMap`
+    );
+    return null;
+  }
+
+  return <IconComponent size={size} className={`${className}`} />;
+}
+
+export default Icon;
