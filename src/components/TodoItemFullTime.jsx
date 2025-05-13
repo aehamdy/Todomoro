b593e58df48d7804890621e7ed7e5636a03acdf2
AhmedEssam <ae.devverse@gmail.com>
2025-05-13 22:05:06 +0300

    Feat: Add todo creation time and todo checked time

--- src/components/TodoItemFullTime.jsx
+++ src/components/TodoItemFullTime.jsx
@@ -1,9 +1,25 @@
+import Icon from "./Icon";
+
 /* eslint-disable react/prop-types */
-function TodoItemFullTime({ todo }) {
+function TodoItemFullTime({ isTodoChecked, timing }) {
   return (
-    <p className="text-xs text-gray-400">
-      {todo.date} - {todo.time}
-    </p>
+    <div className="flex items-center gap-2 text-xs text-gray-400">
+      <div className="flex items-center gap-1">
+        <Icon name="list-plus" size="15" />
+        <p className="">
+          {timing?.createdAt.date} - {timing?.createdAt.time}
+        </p>
+      </div>
+
+      {isTodoChecked && (
+        <div className="flex items-center gap-1">
+          <Icon name="list-check" size="15" />
+          <p className="text-xs text-gray-400">
+            {timing?.checkedAt?.date} - {timing?.checkedAt?.time}
+          </p>
+        </div>
+      )}
+    </div>
   );
 }
 
