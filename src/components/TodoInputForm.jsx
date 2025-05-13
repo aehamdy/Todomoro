ec3479ac4c1f8d08d844dd4b44f77d6f2e12e88c
AhmedEssam <ae.devverse@gmail.com>
2025-05-13 19:48:29 +0300

    Feat: Handle adding task using dispatch method from readux, cleanup and get rid of unused code

--- src/components/TodoInputForm.jsx
+++ src/components/TodoInputForm.jsx
@@ -1,90 +1,32 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import TodoCategoryOptions from "./TodoCategoryOptions";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
 
-function TodoInputForm(props) {
-  const {
-    setTodos,
-    allTodos,
-    setAllTodos,
-    save,
-    setLeftTodos,
-    selectedCategory,
-  } = props;
+function TodoInputForm() {
   const [userInput, setUserInput] = useState("");
   const [inputError, setInputError] = useState(false);
-  const [category, setCategory] = useState("personal");
   const inputRef = useRef();
+  const dispatch = useDispatch();
+
+  const handleOnAddClick = () => {
+    if (userInput.trim()) {
+      dispatch(addTodo(userInput));
+      setInputError("");
+      setUserInput("");
+      inputRef.current.focus();
+    }
+  };
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      handleClick();
-    }
-  };
-
-  const getDate = () => {
-    const date = new Date();
-    const day = date.getDate();
-    const month = date.getMonth();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-
-    return `${day}/${month} ${hours}:${minutes}`;
-  };
-
-  const handleClick = () => {
-    if (userInput.trim()) {
-      setInputError(false);
-
-      const time = getDate();
-
-      //create new todo and merge all todos with it
-      const newTodos = [
-        {
-          id: Date.now(),
-          value: userInput,
-          category: category,
-          isChecked: false,
-          time: time,
-        },
-        ...allTodos,
-      ];
-
-      //update todos list
-      setAllTodos(newTodos);
-      save(newTodos);
-
-      //filter todos depending on selected category
-      const filteredTodos =
-        selectedCategory === "all"
-          ? newTodos
-          : newTodos.filter((todo) => todo.category === selectedCategory);
-      setTodos(filteredTodos);
-
-      //update remaining todos count
-      const leftCount = filteredTodos.filter((todo) => !todo.isChecked).length;
-      setLeftTodos(leftCount);
-
-      setUserInput("");
-    } else {
-      setInputError(true);
-    }
-
-    inputRef.current.focus();
-  };
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
   return (
     <section className="flex flex-col select-none">
       <div className="overflow-x-hidden flex flex-col items-center lg:justify-between lg:items-center gap-2 md:gap-4 py-2 mb-2 border-2 border-solid rounded-lg">
-        <TodoCategoryOptions setCategory={setCategory} />
+        <TodoCategoryOptions />
         <div className="flex justify-center items-center">
           <div className="relative">
             <label
@@ -94,15 +36,14 @@ function TodoInputForm(props) {
               <input
                 type="text"
                 id="taskInput"
-                className="py-1 ps-3 pe-20 h-10 w-full text-black bg-[#fffafa] border-0 rounded-2xl focus:outline-none transition duration-medium caret-black"
+                className="h-10 w-full py-1 ps-3 pe-20 text-black bg-[#fffafa] border-0 rounded-2xl focus:outline-none transition duration-medium caret-black"
                 value={userInput}
                 onChange={handleInputChange}
-                onKeyDown={handleKeyPress}
                 ref={inputRef}
               />
               <button
                 type="button"
-                onClick={handleClick}
+                onClick={handleOnAddClick}
                 className="ms-2 absolute end-0 flex justify-center h-10 py-2 px-6 rounded-2xl bg-blue-400 hover:bg-blue-500 text-white transition duration-medium"
               >
                 Add
