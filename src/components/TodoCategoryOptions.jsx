92a3ea3a71e3f663d902e241b1bc3285802613ac
AhmedEssam <ae.devverse@gmail.com>
2025-05-13 19:03:26 +0300

    Feat: Create TodoCategoryOptions component

--- src/components/TodoCategoryOptions.jsx
+++ src/components/TodoCategoryOptions.jsx
@@ -1,13 +1,7 @@
 /* eslint-disable react/prop-types */
-import { categoryOptions } from "../constants";
-
-const options = [
-  ...categoryOptions.map((option) => ({
-    ...option,
-    name: "category-option",
-    id: option.id + "-option",
-  })),
-];
+import { categoryTypes } from "../constants";
+import categories from "../data/categoryOptions";
+import TodoCategoryOption from "./TodoCategoryOption";
 
 function TodoCategoryOptions(props) {
   const { setCategory } = props;
@@ -19,26 +13,17 @@ function TodoCategoryOptions(props) {
   return (
     <div className="w-full">
       <ul className="categories-options-scrollbar overflow-x-auto flex justify-between items-center gap-1 py-1 px-2 md:py-2 md:px-4">
-        {options.map((option) => (
-          <li key={option.id} className="flex items-center">
-            <input
-              type="radio"
-              name={`${option.name}`}
-              id={option.id}
-              value={option.value}
-              onChange={handleCategorySelection}
-              className="peer hidden"
-              defaultChecked={option.value === "personal"}
-            />
-            <label
-              htmlFor={option.id}
-              className="inline-flex items-center justify-between gap-2 w-full p-1.5 md:p-2 text-gray-500 hover:text-black peer-checked:text-blue-500 peer-checked:font-bold bg-white hover:bg-gray-100 border peer-checked:bg-gray-100 border-gray-200 hover:border-gray-300 peer-checked:border-blue-300 rounded-lg cursor-pointer"
-            >
-              <span>{option.icon}</span>
-              {option.value.charAt(0).toUpperCase() + option.value.slice(1)}
-            </label>
-          </li>
-        ))}
+        {categories.map((option) => {
+          return (
+            option.value.toLowerCase() !== categoryTypes.ALL && (
+              <TodoCategoryOption
+                key={option.id}
+                option={option}
+                handleCategorySelection={handleCategorySelection}
+              />
+            )
+          );
+        })}
       </ul>
     </div>
   );
