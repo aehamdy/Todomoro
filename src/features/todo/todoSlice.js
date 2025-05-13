f15ea41cea50a7bd5343080c58314bcb3e344979
AhmedEssam <ae.devverse@gmail.com>
2025-05-13 18:52:39 +0300

    Add time, date and  selectedCategory action creators

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,18 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import getDateAndTime from "../../utils/getDateAndTime";
+import { categoryTypes } from "../../constants";
 
 const initialState = {
   list: [
     {
       id: 1,
       content: "Task 1",
+      category: "personal",
+      date: `May 09`,
+      time: "05:37",
       isChecked: true,
     },
     {
       id: 2,
       content: "Task 2",
+      category: "work",
+      date: `May 12`,
+      time: "11:50",
       isChecked: false,
     },
   ],
+  selectedCategory: categoryTypes.PERSONAL,
 };
 
 const todoSlice = createSlice({
@@ -20,10 +29,16 @@ const todoSlice = createSlice({
   initialState,
   reducers: {
     addTodo: (state, action) => {
+      const { fullDate, fullTime } = getDateAndTime();
+
       const todo = {
         id: state.list.length + 1,
         content: action.payload,
+        category: state.selectedCategory,
+        time: fullTime,
+        date: fullDate,
       };
+
       state.list.unshift(todo);
     },
 
@@ -38,9 +53,14 @@ const todoSlice = createSlice({
           : todo
       );
     },
+
+    setSelectedCategory: (state, action) => {
+      state.selectedCategory = action.payload;
+    },
   },
 });
 
-export const { addTodo, removeTodo, toggleTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, toggleTodo, setSelectedCategory } =
+  todoSlice.actions;
 
 export default todoSlice.reducer;
