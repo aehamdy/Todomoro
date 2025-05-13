af8f905338e46e13a705b9bcc8342212af80fe2d
AhmedEssam <ae.devverse@gmail.com>
2025-05-13 19:03:49 +0300

    Feat: Create TodoCategoryOption component

---
+++ src/components/TodoCategoryOption.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+import { useDispatch, useSelector } from "react-redux";
+import { categoryTypes } from "../constants";
+import CategoryLabel from "./CategoryLabel";
+import Icon from "./Icon";
+import { setSelectedCategory } from "../features/todo/todoSlice";
+
+function TodoCategoryOption({ option }) {
+  const selectedCategory = useSelector((state) => state.todos.selectedCategory);
+  const dispatch = useDispatch();
+
+  const handleCategorySelection = () => {
+    dispatch(setSelectedCategory(option.value));
+  };
+
+  return (
+    <li key={option.id} className="flex items-center">
+      <label
+        htmlFor={option.id}
+        className={`inline-flex items-center justify-between gap-2 w-full p-1.5 md:p-2 ${
+          selectedCategory === option.value
+            ? "font-bold text-stone-700 bg-blue-100 border-blue-500"
+            : "text-stone-500 bg-white hover:bg-gray-100 border-gray-100"
+        } border rounded-lg cursor-pointer`}
+      >
+        <input
+          type="radio"
+          name="category-option"
+          id={option.id}
+          className="hidden"
+          value={option.value}
+          onChange={handleCategorySelection}
+          defaultChecked={option.value.toLowerCase() === categoryTypes.PERSONAL}
+        />
+
+        <Icon name={option.icon} />
+
+        <CategoryLabel label={option.value} />
+      </label>
+    </li>
+  );
+}
+
+export default TodoCategoryOption;
