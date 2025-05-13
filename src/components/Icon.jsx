9e91280b2c0eb322c65410fb6eee3dee692ee0be
AhmedEssam <ae.devverse@gmail.com>
2025-05-13 06:30:00 +0300

    Chore: Add edit icon and category option icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,14 +1,32 @@
-import { LuCircle, LuCircleCheckBig, LuTrash2 } from "react-icons/lu";
-
 /* eslint-disable react/prop-types */
+import { BiSolidEdit } from "react-icons/bi";
+import {
+  LuBookText,
+  LuBriefcaseBusiness,
+  LuCircle,
+  LuCircleCheckBig,
+  LuFileText,
+  LuList,
+  LuTrash2,
+  LuUserRound,
+} from "react-icons/lu";
+
 const iconMap = {
   trash: LuTrash2,
+  edit: BiSolidEdit,
   emptyCircle: LuCircle,
   checkedCircle: LuCircleCheckBig,
+  //categories options
+  all: LuList,
+  user: LuUserRound,
+  briefcase: LuBriefcaseBusiness,
+  book: LuBookText,
+  list: LuFileText,
 };
 
-function Icon({ name, size, className = "" }) {
+function Icon({ name, size = 18, className = "" }) {
   const IconComponent = iconMap[name];
+  const defaultStyles = "duration-short";
 
   if (!IconComponent) {
     console.warn(
@@ -17,7 +35,9 @@ function Icon({ name, size, className = "" }) {
     return null;
   }
 
-  return <IconComponent size={size} className={`${className}`} />;
+  return (
+    <IconComponent size={size} className={`${className} ${defaultStyles}`} />
+  );
 }
 
 export default Icon;
