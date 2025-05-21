623e2aefd03697ab48d235a7cb958349d3aaeea5
AhmedEssam <ae.devverse@gmail.com>
2025-05-21 22:40:11 +0300

    Feat: Create appConfig that contains app data and app initial state

---
+++ src/config/appConfig.js
@@ -0,0 +1,11 @@
+const appConfig = {
+  name: "Todomoro",
+  LOCAL_STORAGE_KEY: "Todomoro-AppState",
+};
+
+export const initialAppState = {
+  todoList: [],
+  theme: "light",
+};
+
+export default appConfig;
