80a5daa3bf71b2cb427fa3b7f7ebec3a7602b63a
AhmedEssam <ae.devverse@gmail.com>
2025-05-22 03:39:23 +0300

    Feat: Update local storage after each change

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todo/todoSlice";
+import appConfig from "../config/appConfig";
+import addToLocalStorage from "../utils/addToLocalStorage";
 
 const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+
+  addToLocalStorage(appConfig.LOCAL_STORAGE_KEY, state.todos);
 });
 
 export default store;
