2b36a5bf45019b6b0b57e088a9cddc389becb89d
AhmedEssam <ae.devverse@gmail.com>
2025-05-22 03:31:13 +0300

    Chore: Render TodoFilterButton component

--- src/components/TodoFilterButtons.jsx
+++ src/components/TodoFilterButtons.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import categories from "../data/categoryOptions";
-import TodoCategoryButton from "./TodoFilterButton";
+import TodoFilterButton from "./TodoFilterButton";
 
 function TodoFilterButtons() {
   return (
     <ul className="categories-scrollbar flex justify-between gap-1 p-1 mb-2 overflow-x-auto">
       {categories.map((button) => (
-        <TodoCategoryButton key={button.id} button={button} />
+        <TodoFilterButton key={button.id} button={button} />
       ))}
     </ul>
   );
