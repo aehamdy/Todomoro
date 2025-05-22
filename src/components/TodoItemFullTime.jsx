5467202e3136f80ea29813f1c5d5efaeba8feaa0
AhmedEssam <ae.devverse@gmail.com>
2025-05-22 03:30:18 +0300

    Style: Adjust font size on different screens

--- src/components/TodoItemFullTime.jsx
+++ src/components/TodoItemFullTime.jsx
@@ -1,20 +1,20 @@
+/* eslint-disable react/prop-types */
 import Icon from "./Icon";
 
-/* eslint-disable react/prop-types */
 function TodoItemFullTime({ isTodoChecked, timing }) {
   return (
-    <div className="flex items-center gap-2 text-xs text-gray-400">
+    <div className="flex items-center gap-2 text-[10px] md:text-xs text-gray-400">
       <div className="flex items-center gap-1">
         <Icon name="list-plus" size="15" />
-        <p className="">
-          {timing?.createdAt.date} - {timing?.createdAt.time}
+        <p>
+          {timing?.createdAt?.date} - {timing?.createdAt?.time}
         </p>
       </div>
 
       {isTodoChecked && (
         <div className="flex items-center gap-1">
           <Icon name="list-check" size="15" />
-          <p className="text-xs text-gray-400">
+          <p>
             {timing?.checkedAt?.date} - {timing?.checkedAt?.time}
           </p>
         </div>
