d53313ea4b6faab2752a60b9a9b95dfc333d5d53
AhmedEssam <ae.devverse@gmail.com>
2025-05-23 01:20:30 +0300

    Refactor: Update handling left todos count using 'redux-tookit'

--- src/components/TodoLeftTodos.jsx
+++ src/components/TodoLeftTodos.jsx
@@ -1,6 +1,22 @@
 /* eslint-disable react/prop-types */
-function TodoLeftTodos(props) {
-  const { leftTodos } = props;
+import { useSelector } from "react-redux";
+import { categoryTypes } from "../constants";
+
+function TodoLeftTodos() {
+  const selectedFilter = useSelector((state) => state.todos.selectedFilter);
+  const todosList = useSelector((state) => state.todos.list);
+  const { leftTodos } = getUncheckedCount();
+
+  function getUncheckedCount() {
+    const count =
+      selectedFilter === categoryTypes.ALL
+        ? todosList.filter((todo) => !todo.isChecked)
+        : todosList.filter(
+            (todo) => todo.category === selectedFilter && !todo.isChecked
+          );
+
+    return { leftTodos: count.length };
+  }
 
   return (
     <>
