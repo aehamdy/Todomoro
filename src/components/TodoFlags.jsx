3b34068aa90cb74ffcd862cdd2d25de41501ac04
AhmedEssam <ae.devverse@gmail.com>
2025-05-23 01:22:08 +0300

    Refactor: Update the component and get rid of old code

--- src/components/TodoFlags.jsx
+++ src/components/TodoFlags.jsx
@@ -1,43 +1,12 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react";
 import ClearButton from "./ClearButton";
-import LeftTodos from "./TodoLeftTodos";
+import TodoLeftTodos from "./TodoLeftTodos";
 
-function TodoFlags(props) {
-  const {
-    leftTodos,
-    save,
-    setTodos,
-    todos,
-    allTodos,
-    handleLeftTodos,
-    selectedCategory,
-  } = props;
-
-  useEffect(() => {
-    let filteredTodos;
-    if (selectedCategory === "all") {
-      filteredTodos = allTodos;
-    } else {
-      filteredTodos = allTodos.filter(
-        (todo) => todo.category === selectedCategory
-      );
-    }
-
-    const notCheckedTodos = filteredTodos.filter((todo) => !todo.isChecked);
-    handleLeftTodos(notCheckedTodos.length);
-  }, [selectedCategory, allTodos, handleLeftTodos]);
-
+function TodoFlags() {
   return (
     <section className="flex justify-between p-1">
-      <LeftTodos leftTodos={leftTodos} selectedCategory={selectedCategory} />
-      <ClearButton
-        save={save}
-        todos={todos}
-        setTodos={setTodos}
-        selectedCategory={selectedCategory}
-        handleLeftTodos={handleLeftTodos}
-      />
+      <TodoLeftTodos />
+      <ClearButton />
     </section>
   );
 }
