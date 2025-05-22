45f97c465110f1d7847e84d140da370832bfc99d
AhmedEssam <ae.devverse@gmail.com>
2025-05-23 01:19:21 +0300

    Refactor: Update ClearButton functionality using 'redux-toolkit' functionality

--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -1,16 +1,35 @@
-/* eslint-disable react/prop-types */
+import { useDispatch, useSelector } from "react-redux";
+import { removeCompletedTodos } from "../features/todo/todoSlice";
+import { categoryTypes } from "../constants";
+
 function ClearButton() {
+  const dispatch = useDispatch();
+  const todoList = useSelector((state) => state.todos.list);
+  const selectedFilter = useSelector((state) => state.todos.selectedFilter);
+
+  const handleClick = () => {
+    dispatch(removeCompletedTodos(selectedFilter));
+  };
+
+  const showClearButton = () => {
+    return selectedFilter === categoryTypes.ALL
+      ? todoList.find((todo) => todo.isChecked)
+      : todoList.find(
+          (todo) => todo.category === selectedFilter && todo.isChecked
+        );
+  };
+
   return (
-    // todos.find(checkedTodos) && (
-    <button
-      type="button"
-      // onClick={}
-      className="appearance-none text-warning-color hover:underline select-none"
-    >
-      Clear completed
-    </button>
+    showClearButton() && (
+      <button
+        type="button"
+        onClick={handleClick}
+        className="appearance-none text-warning-color hover:underline select-none"
+      >
+        Clear completed
+      </button>
+    )
   );
-  // );
 }
 
 export default ClearButton;
